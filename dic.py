59f08923638bcf04d500d48a5c8c5f72d988504b
gokulng1 <gokulngkl@gmail.com>
2023-11-28 20:11:46 +0530

    first pratic in dictionary

---
+++ dic.py
@@ -0,0 +1,27 @@
+
+employee_info = {
+    "Name": "gokul",
+    "Last Name": "N",
+    "Address": "india",
+    "Job": {"frontend", "Developer"},
+    "Age": 34,
+    "Hobbies": ("Reading", "Wacthing Movies", "Walking"),
+    2002: "Date of Birth",
+    "Married": False,
+    "Favorite Songs": ["Un Dia", "Blinding Lights", "Shallow"],
+    "Best Friend": {"Name": "rajesh"},
+    "Special One": None
+}
+
+# ----->clear()
+# employee_info.clear()
+# print(employee_info)
+
+# # -----copy()
+
+# shan = employee_info.copy()
+# shan["Best Friend"] = "gokul"
+# print(shan)
+
+# # -----fromkeys()
+
