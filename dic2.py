520cc8630c01bb0503988651b15c532658f78ab6
gokulng1 <gokulngkl@gmail.com>
2023-11-29 00:23:49 +0530

    2 dic

---
+++ dic2.py
@@ -0,0 +1,15 @@
+employee_info = {
+    "Name": "gokul",
+    "Last Name": "N",
+    "Address": "india",
+    "Job": {"frontend", "Developer"},
+    "Age": 34,
+    "Hobbies": ("Reading", "Wacthing Movies", "Walking"),
+    2002: "Date of Birth",
+    "Married": False,
+    "Favorite Songs": ["Un Dia", "Blinding Lights", "Shallow"],
+    "Best Friend": {"Name": "rajesh"},
+    "Special One": None
+}
+
+print(employee_info.values())
